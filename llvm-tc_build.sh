345aa6de94cada69b267d4010e767ab4352639ff
Cryztalized <abenkenari@gmail.com>
2021-09-12 05:22:40 +0700

    Update llvm-tc_build.sh

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Use xRageTC build script as LLVM Build Script.
-git clone https://github.com/xyz-prjkt/llvmTC $(pwd)/llvmTC -b llvm-tc
-cd $(pwd)/llvmTC
+git clone https://github.com/BENKz29/vtcbuild $(pwd)/vtcbuild -b master
+cd $(pwd)/vtcbuild
 bash build-tc.sh
